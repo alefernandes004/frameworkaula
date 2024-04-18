538c70ce79479195ca2fd432fce2eb3a05d77a80
Alessandra Fernandes <145944507+alefernandes004@users.noreply.github.com>
2024-04-17 22:00:08 -0300

    js/excluir/atualizar/inc/dec

---
+++ epictask/js/cadastrar.js
@@ -0,0 +1,55 @@
+document.querySelector("#botao-cadastrar").addEventListener("click", () => {
+
+    const form = document.querySelector("form")
+
+    const tarefa = {
+        titulo : form.titulo.value,
+        descricao: form.descricao.value,
+        pontos: form.pontos.value,
+        status: 0
+    }
+
+    if (validar(tarefa)){
+        console.log(tarefa)
+        let tarefas = JSON.parse(localStorage.getItem("tarefas")) || []
+        tarefas.push(tarefa)
+        localStorage.setItem("tarefas", JSON.stringify(tarefas))
+
+        window.location = "/"
+    }
+    
+
+})
+
+function validar(tarefa) {
+    let valid = true
+    limparErros()
+    if(tarefa.titulo === ""){
+        document.querySelector("#titulo").classList.add("is-error")
+        document.querySelector("#titulo-error").innerText = "O título é obrigatório"
+        valid = false 
+    }
+    if(tarefa.descricao === ""){
+        document.querySelector("#descricao").classList.add("is-error")
+        document.querySelector("#descricao-error").innerText = "A descrição é obrigatória"
+        valid = false
+    }
+    if(tarefa.pontos <= 0){
+        document.querySelector("#pontos").classList.add("is-error")
+        document.querySelector("#pontos-error").innerText = "Pontos devem ser maior que zero"
+        valid = false
+    }
+
+    return valid
+}
+
+function limparErros(){
+
+    document
+        .querySelectorAll(".nes-input.is-error, .nes-textarea.is-error")
+        .forEach(campo => campo.classList.remove("is-error"))
+    
+        document.querySelectorAll("span.is-error")
+        .forEach(span => span.innerText = "")
+}
+
---
+++ epictask/js/carregar.js
@@ -0,0 +1,29 @@
+ console.log("carregar dados")
+
+ const tarefas = JSON.parse(localStorage.getItem("tarefas")) || []
+
+tarefas.forEach( (tarefa) => {
+    const card = document.createElement("div")
+    card.id = tarefa.titulo
+    card.classList.add("nes-container", "with-title", "is-centered")
+    
+
+    card.innerHTML = `
+    <p class="title">${tarefa.titulo}</p>
+            <p>${tarefa.descricao}</p>
+            <a href="#" class="nes-badge is-icon">
+                <span class="is-warning"><i class="nes-icon coin is-small"></i></span>
+                <span class="is-primary">${tarefa.pontos}</span>
+            </a>
+
+            <progress class="nes-progress is-success" value="${tarefa.status}" max="100"></progress>
+            
+              
+             <button onClick="apagar('${tarefa.titulo}')" type="button" class="nes-btn is-error">apagar</button>
+             <button onClick="dec('${tarefa.titulo}')"type="button" class="nes-btn is-primary">-</button>
+             <button onClick="inc('${tarefa.titulo}')" type="button" class="nes-btn is-primary">+</button>
+    `
+    document.querySelector("#lista-de-tarefas").appendChild(card)
+})
+
+atualizar()
